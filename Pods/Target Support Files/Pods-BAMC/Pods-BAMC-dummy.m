656e83d95f6f8cea5a4d778ad5f17985afc429db
Nikita Bhalla <nikitabhalla@bluejeansnet.com>
2016-10-23 15:59:07 +0530

    adding pods

---
+++ Pods/Target Support Files/Pods-BAMC/Pods-BAMC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BAMC : NSObject
+@end
+@implementation PodsDummy_Pods_BAMC
+@end
---
+++ Pods/Target Support Files/Pods-BAMC/Pods-BAMC-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BAMCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BAMCVersionString[];
+
