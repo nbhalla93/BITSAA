aad4128a4a1a4c6a5b21ec607e1689f855db5074
Nikita Bhalla <nikitabhalla@bluejeansnet.com>
2016-10-24 21:06:56 +0530

    Added circular ring pod

--- Pods/Target Support Files/Pods-BAMC/Pods-BAMC-frameworks.sh
+++ Pods/Target Support Files/Pods-BAMC/Pods-BAMC-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SwiftHTTP/SwiftHTTP.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UICircularProgressRing/UICircularProgressRing.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SwiftHTTP/SwiftHTTP.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UICircularProgressRing/UICircularProgressRing.framework"
 fi
